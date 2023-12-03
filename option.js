4b4c3878833032b1c1d151b17e5c4a15f815dc0d
Harsh Baranwal <127023632+88harshbaranwal@users.noreply.github.com>
2023-12-03 11:03:47 +0530

    option.js

--- option.js
+++ option.js
@@ -1,29 +1,89 @@
-const cellNAmePlaceholder = document.querySelector("#active-cell");
+
+const cellNamePlaceholder = document.querySelector("#active-cell");
 const fontSizeInput = document.querySelector("#fontsize");
+const fontFamilyInput = document.querySelector("#fontFamily");
+const form = document.querySelector("#form");
+
+let activeElement = null;
+
 const state = {};
-let activeCellElement = null;
+
 const defaultProperties = {
-    fontfamily:'sans',
-    fontSize:16,
-    color:"#000",
-    backgroundColor: "#fff",
+    fontfamily: 'sans-serif',
+    fontSize: 16,
+    color: "#000000",
+    textalign: "left",
+    backgroundColor: "#ffffff",
     isBold: false,
     isItalic: false,
-    inUnderlined: false
+    isUnderlined: false
 }
-function onCellFocus(event){
-    const elementId = event.target.id;
-    cellNAmePlaceholder.innerText = event.target.id; 
-    activeCellElement = event.target;
-    if(state[elementId]){
-        resetOption(state[elementId]);
-    }else{
 
+
+function onCellFocus(event) {
+    const elementId = event.target.id;
+    cellNamePlaceholder.innerText = elementId;
+    activeElement = event.target;
+    if (state[elementId]) {
+        resetOptions(state[elementId]);
+    } else {
+        resetOptions(defaultProperties);
     }
 }
-function resetOption(optionState){
-    fontSizeInput.value = optionState.fontsize;   
-}
-function onChangeFontSize(){
 
+function resetOptions(optionState) {
+    form.fontFamily.value = optionState.fontFamily;
+    form.fontSize.value = optionState.fontSize;
+    form.textalign.value = optionState.textalign;
+    form.bold.checked = optionState.isBold;
+    form.italic.checked = optionState.isItalic;
+    form.underlined.checked = optionState.isUnderlined;
+    form.textcolor.value = optionState.color;
+    form.bgcolor.value = optionState.backgroundColor;
+}
+
+function onFormChange() {
+    if (!activeElement) {
+        alert("Please select a cell to make changes");
+        form.reset();
+        return;
+    }
+
+    let currentState = {
+        textColor: form.textcolor.value,
+        backgroundColor: form.bgcolor.value,
+        fontSize: form.fontSize.value,
+        fontFamily: form.fontFamily.value,
+        isBold: form.bold.checked,
+        isItalic: form.italic.checked,
+        isUnderlined: form.underlined.checked,
+        textalign: form.textalign.value
+
+    }
+    applyStylesToCell(currentState);
+
+    //update the state object for the current cell.
+    state[activeElement.id] = currentState;
+
+}
+
+function applyStylesToCell(styleObject) {
+    console.log(styleObject);
+    // it will take the style object and applies it to the currently selected cell.
+    activeElement.style.fontSize = `${styleObject.fontSize}`;
+    activeElement.style.fontFamily = styleObject.fontFamily;
+    activeElement.style.color = styleObject.color;
+    activeElement.style.backgroundColor = styleObject.backgroundColor;
+    activeElement.style.textAlign = styleObject.textalign;
+    if(styleObject.isItalic){
+        activeElement.style.fontStyle = "italic";    
+    }
+    if(styleObject.isBold){
+        activeElement.style.fontWeight = "bold";
+    }
+    if(styleObject.isUnderlined){
+        activeElement.style.textDecoration = "underline";
+    }
+
+    
 }
